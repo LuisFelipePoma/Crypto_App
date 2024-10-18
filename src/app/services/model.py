efeb9679e8de0566ca2f3abdfe03b6398d76d90a
don_gato <102071960+LuisFelipePoma@users.noreply.github.com>
2024-10-18 05:13:23 +0000

    pepe3

--- src/app/services/model.py
+++ src/app/services/model.py
@@ -45,50 +45,50 @@ def calculate_macd(df, slow=26, fast=12, signal=9):
     return df["macd"] - df["signal"]
 
 
-class ClassifierRandomCoin:
-    def __init__(self):
-        self.model = joblib.load("../models/random_forest.joblib")
-        self.scaler = joblib.load("../models/scaler_forest.joblib")
+# class ClassifierRandomCoin:
+#     def __init__(self):
+#         self.model = joblib.load("../models/random_forest.joblib")
+#         self.scaler = joblib.load("../models/scaler_forest.joblib")
 
-    def __get_features(self, df: pd.DataFrame):
-        features = {}
-        # preprocess
-        # feature engineering
-        df["log_return"] = np.log(df["close"] / df["close"].shift(1))
-        features["sharper"] = calculate_sharper(df)
-        df["volatility"] = (df["high"] - df["low"]) / df["low"]
-        features["volatility"] = df["volatility"].mean()
-        features["log_return"] = df["log_return"].mean()
-        features["mean_close"] = df["close"].mean()
-        features["std_close"] = df["close"].std()
-        features["max_close"] = df["close"].max()
-        features["min_close"] = df["close"].min()
-        features["mean_volume"] = df["volume"].mean()
-        features["std_volume"] = df["volume"].std()
-        df["rsi"] = calculate_rsi(df["close"])
-        features["mean_rsi"] = df["rsi"].mean()
-        features["std_rsi"] = df["rsi"].std()
-        df["macd"] = calculate_macd(df)
-        features["mean_macd"] = df["macd"].mean()
-        features["std_macd"] = df["macd"].std()
-        return pd.DataFrame(features, index=[0])
+#     def __get_features(self, df: pd.DataFrame):
+#         features = {}
+#         # preprocess
+#         # feature engineering
+#         df["log_return"] = np.log(df["close"] / df["close"].shift(1))
+#         features["sharper"] = calculate_sharper(df)
+#         df["volatility"] = (df["high"] - df["low"]) / df["low"]
+#         features["volatility"] = df["volatility"].mean()
+#         features["log_return"] = df["log_return"].mean()
+#         features["mean_close"] = df["close"].mean()
+#         features["std_close"] = df["close"].std()
+#         features["max_close"] = df["close"].max()
+#         features["min_close"] = df["close"].min()
+#         features["mean_volume"] = df["volume"].mean()
+#         features["std_volume"] = df["volume"].std()
+#         df["rsi"] = calculate_rsi(df["close"])
+#         features["mean_rsi"] = df["rsi"].mean()
+#         features["std_rsi"] = df["rsi"].std()
+#         df["macd"] = calculate_macd(df)
+#         features["mean_macd"] = df["macd"].mean()
+#         features["std_macd"] = df["macd"].std()
+#         return pd.DataFrame(features, index=[0])
 
-    def __prepare_input(self, data: list[dict]):
-        df = pd.DataFrame(data)
-        features = self.__get_features(df)
-        # scale data
-        return self.scaler.transform(features)
+#     def __prepare_input(self, data: list[dict]):
+#         df = pd.DataFrame(data)
+#         features = self.__get_features(df)
+#         # scale data
+#         return self.scaler.transform(features)
 
-    def predict(self, data: list[dict]):
-        x = self.__prepare_input(data)
-        prediction = self.model.predict(x)
-        print(prediction)
-        return prediction[0]
+#     def predict(self, data: list[dict]):
+#         x = self.__prepare_input(data)
+#         prediction = self.model.predict(x)
+#         print(prediction)
+#         return prediction[0]
     
-    def features(self, data: list[dict]):
-        x = pd.DataFrame(data)
-        features = self.__get_features(x)
-        return features.to_dict(orient="records")[0]
+#     def features(self, data: list[dict]):
+#         x = pd.DataFrame(data)
+#         features = self.__get_features(x)
+#         return features.to_dict(orient="records")[0]
 
 
 # --------------------------------------------------------------
