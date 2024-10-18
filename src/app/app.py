b67587ed9069dd3954ac5fd6d97b367ad16433ed
don_gato <102071960+LuisFelipePoma@users.noreply.github.com>
2024-10-18 06:21:18 +0000

    pepe

--- src/app/app.py
+++ src/app/app.py
@@ -55,4 +55,5 @@ def low():
 # <--------------- MAIN
 if __name__ == "__main__":
     coins = Coins()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run()
